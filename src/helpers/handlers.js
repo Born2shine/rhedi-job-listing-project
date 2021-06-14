43b2cefdb4b5deebd002d3902afee2979e4e40a2
SpiderTech <odohfriday33@yahoo.com>
2021-06-15 00:39:29 +0100

    added some css

--- src/helpers/handlers.js
+++ src/helpers/handlers.js
@@ -3,14 +3,12 @@
 const findJob = (jobs, slug) => jobs.find((job) => job.slug === slug)
 
 const chunkJob = (jobs) => {
-    const itemsPerPage = 1;
+    const itemsPerPage = 10;
     const pages = Math.ceil(jobs.length / itemsPerPage)
     const newJobs = Array.from({ length: pages }, (_, index) => {
         const start = index * itemsPerPage
-        // console.log(start)
         return jobs.slice(start, start + itemsPerPage)
     })
-    // console.log(newJobs)
     return newJobs
 }
 
