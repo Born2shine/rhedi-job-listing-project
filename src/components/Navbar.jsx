a4c0ccb52d935f6701c9530c23f3f07c5004e502
SpiderTech <odohfriday33@yahoo.com>
2021-06-17 12:48:06 +0100

    changed dark mode icon color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const  Navbar = () => {
         <div className="nav">
             <Link to="/" onClick={openFilterHandler}><h2>devjobs</h2></Link>
             <div className="toggle">
-                <span className="toggle-icon"><i className="fas fa-sun"></i></span>
+                <span className="toggle-icon" style={darkMode ? {color: '#ffc107', borderBottom: '2px solid #ffc107'} : {}}><i className="fas fa-sun"></i></span>
                 <span className="toggle-icon" onClick={ToggleDarkMode}><i className={`${darkMode ? "fas fa-toggle-on" : "fas fa-toggle-off"}`}></i></span>
-                <span className="toggle-icon"><i className="fas fa-moon"></i></span>
+                <span className="toggle-icon" style={darkMode ? {} : {color: '#ffc107', borderBottom: '2px solid #ffc107'}}><i className="fas fa-moon"></i></span>
             </div>
         </div>
     )
